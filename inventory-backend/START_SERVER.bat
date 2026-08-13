47fa4a146366496abc3bdf03b216a43a3afe419d
anaslotfysalemhamza-bit <anas.lotfysalem.hamza@gmail.com>
2026-08-14 00:39:29 +0300

    Delete inventory-backend directory

--- inventory-backend/START_SERVER.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Inventory Backend Server
-cd /d "%~dp0"
-echo ====================================
-echo    Starting Inventory Backend Server
-echo ====================================
-echo.
-npm start
